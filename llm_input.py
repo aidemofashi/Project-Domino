fd56472004df587da7d0c319ba6b3190c0420caf
aidemofashi <zhonghuaiyu2019@163.com>
2026-02-06 18:53:49 +0800

    实现对话保存和传输到大模型的api

---
+++ llm_input.py
@@ -0,0 +1,31 @@
+import openai
+# 本地 API 地址
+openai.api_base = "http://192.168.48.1:8888/v1" # LM Studio 默认端口
+openai.api_key = " " # 可随意填写，本地不会校验
+
+class llm_input:
+    def send_llm(input):
+            # input 应该是列表，且内部是 dict
+        if not isinstance(input, list):
+            raise TypeError("input(messages) 应为 list")
+        if not all(isinstance(msg, dict) for msg in input):
+            raise TypeError("messages 每项都需为 dict, 请检查 chat.json 数据格式！")
+        response = openai.ChatCompletion.create(
+        model="qwen/qwen3-4b-2507b", # 必须与 LM Studio 中加载的模型名称一致
+        messages=input,
+        temperature=0.6,
+        max_tokens=200,
+        stream=True,
+        )
+        full_reply = ""
+        for chunk in response:
+            if "choices" in chunk and len(chunk["choices"]) > 0:
+                delta = chunk["choices"][0].get("delta", {})
+                content = delta.get("content", "")
+                if content:
+                    print(content, end="", flush=True)
+                    full_reply += content
+
+        print()
+        return full_reply
+        # 如果后续要用完整回复，可用 full_reply
--- start_cpu.py
+++ start_cpu.py
@@ -2,10 +2,11 @@ import os
 import json
 from funasr import AutoModel
 import time
-from audio_input import AudioInput
-from filter import Filter
 import keyboard
-
+#文件
+from audio_input import AudioInput  #语音输入  
+from filter import Filter
+from llm_input import llm_input
 
 #路径处理,当前先不用
 dir = "./"
@@ -14,7 +15,7 @@ s = 0
 model = AutoModel(
     model="iic/SenseVoiceSmall",
     vad_model="iic/speech_fsmn_vad_zh-cn-16k-common-pytorch",
-    device="cpu",
+    device="cuda",
     disable_pbar=True,
     disable_update=True)
 
@@ -37,19 +38,31 @@ while True:
         text = res[0]['text']
         print(f"识别结果：{text}")
         # 1. 准备数据
+        date_time = time.strftime("%Y-%m-%d %H:%M:%S")
         res[0]['datetime'] = time.strftime("%Y-%m-%d %H:%M:%S")
         res[0].pop("key", None)
 
         # 2. 读取并累加
-        filename = "output_full.json"
-        if os.path.exists(filename) and os.path.getsize(filename) > 0:
-            with open(filename, 'r', encoding='utf-8') as file:
+        audio_input_file = "output_full.json"
+        chat_file = "chat.json"
+
+        if os.path.exists(audio_input_file) and os.path.getsize(audio_input_file) and os.path.exists(chat_file) and os.path.getsize(audio_input_file) > 0:
+            with open(audio_input_file, 'r', encoding='utf-8') as file:
                 contents = json.load(file)
+            with open(chat_file, 'r', encoding='utf-8') as file:
+                chat = json.load(file)
         else:
             contents = []
+            chat = []
         if Filter.emo(res[0]['text']) != False:
             contents.append(res[0])
-
+            # chat.append('"role": "Aidemofashi", "content": '+ res[0]['text'] + "/no_think")  # 错误
+            chat.append({"role": "user", "content": res[0]['text'] + "/no_think","time" : date_time})      # 正确
+            # 讲识别的发送到llm
+            response = llm_input.send_llm(chat)
+            chat.append({"role": "assistant","content": response,"time" : date_time})
         # 3. 保存
-        with open(filename, 'w', encoding='utf-8') as file:
+        with open(audio_input_file, 'w', encoding='utf-8') as file:
             json.dump(contents, file, ensure_ascii=False, indent=4)
+        with open(chat_file, 'w', encoding='utf-8') as file:
+            json.dump(chat, file, ensure_ascii=False, indent=4)
